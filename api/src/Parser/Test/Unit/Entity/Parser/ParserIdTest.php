8a083be303b1259ce8b29d71abbf8c78be278fa9
Flaaim <flaaim@list.ru>
2026-06-19 13:52:25 +1000

    added ParserId

---
+++ api/src/Parser/Test/Unit/Entity/Parser/ParserIdTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parser\Test\Unit\Entity\Parser;
+
+use App\Parser\Entity\Parser\ParserId;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+final class ParserIdTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $id = new ParserId($value = Uuid::uuid4()->toString());
+
+        self::assertEquals($value, $id->getValue());
+    }
+
+    public function testCase(): void
+    {
+        $value = Uuid::uuid4()->toString();
+
+        $id = new ParserId(mb_strtoupper($value));
+
+        self::assertEquals($value, $id->getValue());
+    }
+
+    public function testGenerate(): void
+    {
+        $id = ParserId::generate();
+
+        self::assertNotEmpty($id->getValue());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new ParserId('12345');
+    }
+
+    public function testEmpty(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new ParserId('');
+    }
+}
