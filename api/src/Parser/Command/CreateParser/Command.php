e7bea0482ffef926d2dab317e384a8d49a1a22e2
Flaaim <flaaim@list.ru>
2026-06-20 09:51:22 +1000

    added assertUrl

--- api/src/Parser/Command/CreateParser/Command.php
+++ api/src/Parser/Command/CreateParser/Command.php
@@ -8,6 +8,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 final class Command
 {
     public function __construct(
+        #[Assert\Url(
+            message: 'The url {{ value }} is not a valid url'
+        )]
         #[Assert\NotBlank]
         public readonly string $host,
         #[Assert\NotBlank]
