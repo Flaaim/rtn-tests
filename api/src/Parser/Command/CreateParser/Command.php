06c609077c4407f7c1c8c01f97676ff2efa7dc89
Flaaim <flaaim@list.ru>
2026-06-19 21:16:51 +1000

    added asserts to create parser command

--- api/src/Parser/Command/CreateParser/Command.php
+++ api/src/Parser/Command/CreateParser/Command.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Parser\Command\CreateParser;
 
+use Symfony\Component\Validator\Constraints as Assert;
 final class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
         public readonly string $host,
+        #[Assert\NotBlank]
         public readonly string $login,
+        #[Assert\NotBlank]
         public readonly string $password,
     ) {}
 }
