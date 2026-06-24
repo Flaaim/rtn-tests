48f473a09da7e036bc8b27e1b4f024fa4144afdb
Flaaim <flaaim@list.ru>
2026-06-24 19:20:25 +1000

    added task status

---
+++ api/src/Parser/Entity/Task/Status.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parser\Entity\Task;
+
+use Webmozart\Assert\Assert;
+
+final class Status
+{
+    public const string PROCESSING = 'processing';
+    public const string COMPLETED = 'completed';
+    public const string FAILED = 'failed';
+
+    public function __construct(
+        private string $value
+    ) {
+        Assert::oneOf($value, [self::PROCESSING, self::COMPLETED, self::FAILED]);
+    }
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+    public static function processing(): self
+    {
+        return new self('processing');
+    }
+
+    public static function completed(): self
+    {
+        return new self('completed');
+    }
+    public static function failed(): self
+    {
+        return new self('failed');
+    }
+}
---
+++ api/src/Parser/Test/Unit/Entity/Task/StatusTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parser\Test\Unit\Entity\Task;
+
+use App\Parser\Entity\Task\Status;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class StatusTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $status = new Status($name = Status::PROCESSING);
+
+        self::assertEquals($name, $status->getValue());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Status('none');
+    }
+
+    public function testProcessing(): void
+    {
+        $status = Status::processing();
+        self::assertEquals('processing', $status->getValue());
+    }
+
+}
