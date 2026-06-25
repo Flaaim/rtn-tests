1c571275a99544099b391ffe841919bb256f996c
Flaaim <flaaim@list.ru>
2026-06-25 15:13:28 +1000

    added parsers table migrations

---
+++ api/src/Data/Migration/Version20260621020837.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260621020837 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE parsers ADD login VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE parsers ADD password VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE parsers DROP login');
+        $this->addSql('ALTER TABLE parsers DROP password');
+    }
+}
