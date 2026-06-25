a3426fb4b1c8b25e3c97239243bffc467d27367d
Flaaim <flaaim@list.ru>
2026-06-25 14:00:37 +1000

    added LaunchParse handler

---
+++ api/src/Parser/Command/LaunchParse/Command.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parser\Command\LaunchParse;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class Command
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
+        public string $parserId,
+        #[Assert\NotBlank]
+        public string $branchId,
+        #[Assert\NotBlank]
+        public string $ticketId
+    )
+    {}
+}
---
+++ api/src/Parser/Command/LaunchParse/Handler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Parser\Command\LaunchParse;
+
+use App\Infrastructure\Doctrine\Flusher;
+use App\Parser\Entity\Parser\ParserId;
+use App\Parser\Entity\Task\Task;
+use App\Parser\Entity\Task\TaskId;
+use App\Parser\Entity\Task\TasksRepository;
+
+final class Handler
+{
+    public function __construct(
+        private readonly Flusher $flusher,
+        private readonly TasksRepository $tasks
+    ) {}
+
+    public function handle(Command $command): void
+    {
+        $task = new Task(
+            TaskId::generate(),
+            new ParserId($command->parserId),
+            $command->branchId,
+            $command->ticketId,
+        );
+
+        $this->tasks->add($task);
+
+        $this->flusher->flush();
+
+    }
+}
