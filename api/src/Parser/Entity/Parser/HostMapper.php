24dcb774d5361a94484df97ee2bf704d0dff273a
Flaaim <flaaim@list.ru>
2026-06-26 22:36:21 +1000

    added cases questions, answers

--- api/src/Parser/Entity/Parser/HostMapper.php
+++ api/src/Parser/Entity/Parser/HostMapper.php
@@ -7,4 +7,6 @@ namespace App\Parser\Entity\Parser;
 enum HostMapper: string
 {
     case AUTH = 'Auth/Admin';
+    case PATH_QUESTIONS = 'Admin/Info/GetTicketInfo/';
+    case PATH_ANSWERS = 'Admin/Archive/QuestionInfo';
 }
