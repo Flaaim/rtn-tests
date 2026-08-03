52cf64f190f937a8b3c6e18c256d79299a485875
Flaaim <Flaaim@list.ru>
2026-08-04 08:32:05 +1000

    added CourseStatus VO

---
+++ api/src/Testing/Entity/CourseStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Testing\Entity;
+
+use Webmozart\Assert\Assert;
+
+final class CourseStatus
+{
+    public const string ACTIVE = 'active';
+    public const string INACTIVE = 'inactive';
+
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        Assert::oneOf($value, [self::ACTIVE, self::INACTIVE]);
+        $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public static function active(): self
+    {
+        return new self(self::ACTIVE);
+    }
+
+    public static function inactive(): self
+    {
+        return new self(self::INACTIVE);
+    }
+
+    public function isActive(): bool
+    {
+        return self::ACTIVE === $this->value;
+    }
+
+    public function isInactive(): bool
+    {
+        return self::INACTIVE === $this->value;
+    }
+}
---
+++ api/src/Testing/Test/Unit/Entity/CourseStatusTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Testing\Test\Unit\Entity;
+
+use App\Testing\Entity\CourseStatus;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class CourseStatusTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $status = new CourseStatus(CourseStatus::ACTIVE);
+
+        self::assertEquals(CourseStatus::ACTIVE, $status->getValue());
+    }
+
+    public function testInvalid(): void
+    {
+        self::expectException(InvalidArgumentException::class);
+        new CourseStatus('invalid');
+    }
+
+    public function testInactive(): void
+    {
+        $status = new CourseStatus(CourseStatus::INACTIVE);
+
+        self::assertTrue($status->isInactive());
+        self::assertFalse($status->isActive());
+    }
+
+    public function testActive(): void
+    {
+        $status = new CourseStatus(CourseStatus::ACTIVE);
+
+        self::assertTrue($status->isActive());
+        self::assertFalse($status->isInactive());
+    }
+}
