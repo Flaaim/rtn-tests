bfb4b4c37c11fa000559c314a3332d425f0c1bfb
Flaaim <Flaaim@list.ru>
2026-08-13 09:00:12 +1000

    added LogOnTestCreatedHandler

---
+++ api/src/Testing/MessageHandler/LogOnTestCreatedHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Testing\MessageHandler;
+
+use App\Testing\Event\TestCreated;
+use Psr\Log\LoggerInterface;
+
+final class LogOnTestCreatedHandler
+{
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {}
+
+    public function __invoke(TestCreated $event): void
+    {
+        $testId = $event->id;
+
+        $this->logger->info('Test created: ', ['testId' => $testId]);
+    }
+}
