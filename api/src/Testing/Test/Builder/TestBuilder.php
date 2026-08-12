66d425f2bbbe837bcf2b60e9661ce9f527b59806
Flaaim <Flaaim@list.ru>
2026-08-12 20:29:29 +1000

    added TestBuilder

---
+++ api/src/Testing/Test/Builder/TestBuilder.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Testing\Test\Builder;
+
+use App\Testing\Entity\DTO\TicketDTO;
+use App\Testing\Entity\Status;
+use App\Testing\Entity\Test;
+use App\Testing\Entity\TestId;
+use DateTimeImmutable;
+
+final class TestBuilder
+{
+    private TestId $id;
+    private string $name;
+    private string $description;
+    private string $cipher;
+    private int $allowedMistakes;
+    private string $courseId;
+    private array $tickets;
+    private Status $status;
+    private string $slug;
+    private DateTimeImmutable $createdAt;
+
+    public function __construct(
+    ) {
+        $this->id = new TestId('6ed7c3cb-b8ea-4615-8cfe-67b389a2d193');
+        $this->name = 'Первая помощь';
+        $this->cipher = 'ОТ 201.18';
+        $this->description = 'Test description';
+        $this->allowedMistakes = 2;
+        $this->courseId = '0121b081-c461-42f0-b8ec-a4632a64faea';
+        $this->tickets = [];
+        $this->status = Status::inactive();
+        $this->slug = 'ot201';
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function withId(TestId $id): self
+    {
+        $clone = clone $this;
+        $clone->id = $id;
+        return $clone;
+    }
+
+    public function withName(string $name): self
+    {
+        $clone = clone $this;
+        $clone->name = $name;
+        return $clone;
+    }
+
+    public function withDescription(string $description): self
+    {
+        $clone = clone $this;
+        $clone->description = $description;
+        return $clone;
+    }
+
+    public function withAllowedMistakes(int $allowedMistakes): self
+    {
+        $clone = clone $this;
+        $clone->allowedMistakes = $allowedMistakes;
+        return $clone;
+    }
+
+    public function withCourseId(string $courseId): self
+    {
+        $clone = clone $this;
+        $clone->courseId = $courseId;
+        return $clone;
+    }
+
+    /** @param TicketDTO[] $tickets */
+    public function withTickets(array $tickets): self
+    {
+        $clone = clone $this;
+        $clone->tickets = $tickets;
+        return $clone;
+    }
+
+    public function withStatus(Status $status): self
+    {
+        $clone = clone $this;
+        $clone->status = $status;
+        return $clone;
+    }
+
+    public function withSlug(string $slug): self
+    {
+        $clone = clone $this;
+        $clone->slug = $slug;
+        return $clone;
+    }
+
+    public function build(): Test
+    {
+        return new Test(
+            $this->id,
+            $this->name,
+            $this->description,
+            $this->cipher,
+            $this->allowedMistakes,
+            $this->courseId,
+            $this->tickets,
+            $this->status,
+            $this->slug,
+            $this->createdAt
+        );
+    }
+}
