18f5f89bad53cb07c620c4b13c5905481e0b03cd
Flaaim <Flaaim@list.ru>
2026-08-12 19:43:54 +1000

    added Test getters

--- api/src/Testing/Entity/Test.php
+++ api/src/Testing/Entity/Test.php
@@ -28,4 +28,44 @@ final class Test
     {
         return $this->status;
     }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getCourseId(): string
+    {
+        return $this->courseId;
+    }
+
+    public function getAllowedMistakes(): int
+    {
+        return $this->allowedMistakes;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getTickets(): array
+    {
+        return $this->tickets;
+    }
+
+    public function getQuestions(): array
+    {
+        return $this->questions;
+    }
+
+    public function getCipher(): string
+    {
+        return $this->cipher;
+    }
 }
