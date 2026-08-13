0c91d66e0d1443a478bafe4677db9173af95ce10
Flaaim <Flaaim@list.ru>
2026-08-13 16:53:50 +1000

    moved handler to Test directory

--- api/src/Testing/Command/Test/Add/Command.php
+++ api/src/Testing/Command/Test/Add/Command.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Testing\Command\Add;
+namespace App\Testing\Command\Test\Add;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
--- api/src/Testing/Command/Test/Add/Handler.php
+++ api/src/Testing/Command/Test/Add/Handler.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Testing\Command\Add;
+namespace App\Testing\Command\Test\Add;
 
 use App\Course\Api\CourseApi;
 use App\Infrastructure\Doctrine\Flusher;
