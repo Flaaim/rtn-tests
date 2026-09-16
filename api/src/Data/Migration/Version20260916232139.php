2010a00c8e890e4e151f8ff5eb0b56b2ecc6a258
Flaaim <Flaaim@list.ru>
2026-09-17 09:23:18 +1000

    added Subscription migration

---
+++ api/src/Data/Migration/Version20260916232139.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260916232139 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE subscriptions (id VARCHAR NOT NULL, user_id VARCHAR(255) NOT NULL, duration_days INT NOT NULL, plan VARCHAR(16) NOT NULL, status VARCHAR(16) NOT NULL, period_start TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, period_end TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE subscriptions');
+    }
+}
